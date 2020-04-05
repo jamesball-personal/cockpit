801840f242dca568d4defbdd9433c47cbe5e93fd
jamesball-personal <jamesball.personal@gmail.com>
2020-04-05 12:13:25 +0100

    Add getSnapshotId method to PlanItem entity

--- src/main/java/com/jamesball/datawarehouse/entity/PlanItem.java
+++ src/main/java/com/jamesball/datawarehouse/entity/PlanItem.java
@@ -53,6 +53,10 @@ public class PlanItem {
         }};
     }
 
+    public Long getSnapshotId() {
+        return planItemId.getSnapshot().getId();
+    }
+
     public Long getOwnerPersonnelId() {
         return ownerPersonnelId;
     }
