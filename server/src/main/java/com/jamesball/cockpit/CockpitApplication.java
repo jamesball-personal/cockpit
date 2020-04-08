bbcba3175cabc2ae32b583f50fbe8688121ff656
jamesball-personal <jamesball.personal@gmail.com>
2020-04-08 12:40:38 +0100

    Rename application

--- server/src/main/java/com/jamesball/cockpit/CockpitApplication.java
+++ server/src/main/java/com/jamesball/cockpit/CockpitApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataWarehouseApplication {
+public class CockpitApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DataWarehouseApplication.class, args);
+        SpringApplication.run(CockpitApplication.class, args);
     }
 }
--- server/src/test/java/com/jamesball/cockpit/CockpitApplicationTests.java
+++ server/src/test/java/com/jamesball/cockpit/CockpitApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class DataWarehouseApplicationTests {
+public class CockpitApplicationTests {
 
     @Test
     public void contextLoads() {}
