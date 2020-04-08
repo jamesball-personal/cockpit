73b6f49d3f9b38099f3a49dd28dc1ef2fa1024a9
jamesball-personal <jamesball.personal@gmail.com>
2020-04-08 20:34:50 +0100

    Remove `life_cycle_status` table

--- server/src/main/resources/db/migration/V12__alter_table_plan_item_add_constraint_foreign_key.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE plan_item
-    ADD CONSTRAINT plan_item__fk__life_cycle_status
-        FOREIGN KEY (life_cycle_status) REFERENCES life_cycle_status (id);
--- server/src/main/resources/db/migration/V2__create_table_life_cycle_status.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE life_cycle_status (
-    id BIGINT NOT NULL,
-    name VARCHAR(255) NOT NULL
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-ROW_FORMAT = DYNAMIC;
