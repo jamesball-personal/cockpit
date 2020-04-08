af41e656fb741e671da2ad993d19373228e60e7f
jamesball-personal <jamesball.personal@gmail.com>
2020-04-08 14:31:12 +0100

    Add REST controller integration tests

---
+++ server/src/test/java/com/jamesball/cockpit/controller/CockpitRestControllerIntegrationTests.java
@@ -0,0 +1,41 @@
+package com.jamesball.cockpit.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CockpitRestControllerIntegrationTests {
+
+    @LocalServerPort
+    private Integer port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getAllMetrics() throws Exception {
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:" + port + "/cockpit/metric", List.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void getAllObjectives() throws Exception {
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:" + port + "/cockpit/objective", List.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void getAllProjects() throws Exception {
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:" + port + "/cockpit/project", List.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+}
