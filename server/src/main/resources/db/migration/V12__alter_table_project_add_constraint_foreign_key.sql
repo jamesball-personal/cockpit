9b62d60186a9caeadbbc7e14e0f6d77f261d3334
jamesball-personal <jamesball.personal@gmail.com>
2020-04-09 14:54:45 +0100

    Deploy schema with Hibernate, migrate with Flyway

--- server/src/main/resources/db/migration/V12__alter_table_project_add_constraint_foreign_key.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE project
-    ADD CONSTRAINT project__fk__id
-        FOREIGN KEY (id) REFERENCES plan_item (id);
--- server/src/main/resources/db/migration/V8__alter_table_project_add_constraint_primary_key.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE project
-    CHANGE COLUMN id
-        id BIGINT NOT NULL AUTO_INCREMENT,
-    ADD CONSTRAINT project__pk__id
-        PRIMARY KEY (id);
