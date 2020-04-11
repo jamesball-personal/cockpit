19fac3d2fb6a8c7c2eb1e3d4f95d451d41fae736
Richie Ward <RichieS@GMail.com>
2020-04-11 15:32:49 +0100

    Send results to client

--- client/src/graphQL.ts
+++ client/src/graphQL.ts
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
-    uri: `${window.location.protocol}//${window.location.hostname}:8080`
+    uri: `${window.location.protocol}//${window.location.hostname}:8080/graphql`
 });
