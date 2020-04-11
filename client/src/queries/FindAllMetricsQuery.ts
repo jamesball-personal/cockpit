db570aeb657139465c395403505ee2aabf40f001
Richie Ward <RichieS@GMail.com>
2020-04-11 15:19:28 +0100

    Add find all metrics query, codegen files, create mock directory structure

---
+++ client/src/queries/FindAllMetricsQuery.ts
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag';
+import { useQuery } from "@apollo/react-hooks";
+import { FindAllMetrics } from "../types/FindAllMetrics";
+
+const QUERY = gql`
+    query FindAllMetrics {
+        findAllMetrics {
+            id
+            name
+            description
+            lifeCycleStatus
+        }
+    }
+`;
+
+export const useFindAllMetricsQuery = () => useQuery<FindAllMetrics>(QUERY);
---
+++ client/src/types/globalTypes.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum LifeCycleStatus {
+  ACTIVE = "ACTIVE",
+  CANCELLED = "CANCELLED",
+  COMPLETED = "COMPLETED",
+  EXECUTION_READY = "EXECUTION_READY",
+  PROPOSED = "PROPOSED",
+  SCOPED = "SCOPED",
+  SUSPENDED = "SUSPENDED",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
