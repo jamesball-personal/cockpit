8c759149e32a185e774df45bd60ca12967a0aefb
Richie Ward <RichieS@GMail.com>
2020-04-11 14:56:37 +0100

    Add apollo provider and apollo client. Add missing react-apollo dependency.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
-import {Home} from "./components/Home";
+import React from 'react';
+import { Home } from "./components/Home";
+import ApolloProvider from 'react-apollo/ApolloProvider';
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
+import { client } from "./graphQL";
 
 function App() {
     return (
-        <Home />
+        <ApolloProvider client={client}>
+            <ApolloHooksProvider client={client}>
+                <Home />
+            </ApolloHooksProvider>
+        </ApolloProvider>
+
     );
 }
 
---
+++ client/src/graphQL.ts
@@ -0,0 +1,5 @@
+import ApolloClient from 'apollo-boost';
+
+export const client = new ApolloClient({
+    uri: `${window.location.protocol}//${window.location.hostname}:8080`
+});
